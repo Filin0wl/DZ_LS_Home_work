2f115ef6d485826fb9543a7c32ed5c75119f20dd
Stacy <nastia-dogf@mail.ru>
2020-04-15 03:06:55 +0300

    done

--- app/src/main/java/com/example/dz_ls_home_work/MainActivity.kt
+++ app/src/main/java/com/example/dz_ls_home_work/MainActivity.kt
@@ -2,11 +2,37 @@ package com.example.dz_ls_home_work
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+companion object{
+    var count: Int = 0
 
+}
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        count = savedInstanceState.getInt("count")
+        textView.text= count.toString()
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        if(count > 0){
+            outState.putInt("count",count)
+        }
+
+    }
+
+    fun setCount(view: View) {
+        count += 1
+        textView.text= count.toString()
     }
 }
